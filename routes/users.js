b801962f35b6800250a7d42e9c2f7cc23db21f57
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-24 19:39:33 -0600

    Adds post route to users route for creating new user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../../models').User;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post("/users", function(req,res,next) {
+  User.create({
+    username: req.body.username,
+    password: req.body.password,
+    api_key: Math.random.toString(36).substring(2,8)
+    // the arg for toString is the radix which means what base (binary = 2); 36 includes letters
+    // substring takes the characters from index 2 to 8 not including 8
+  })
+  .then(user => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(201).send(`${user.username} has been created`)
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({error})
+  });
+})
 
 module.exports = router;
