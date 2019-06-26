f4a33cc9956f1b3e8b053a0b530ebb77c5c0a734
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-25 19:30:23 -0600

    Adds bcrypt to users route and moves User.create into bcrypt function

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../../../models').User;
+var bcrypt = require('bcryptjs');
 
 router.post("/", function(req,res,next) {
   var email = req.body.email
@@ -10,21 +11,25 @@ router.post("/", function(req,res,next) {
     res.status(409).send(`Must provide password confirmation.`)
   }
   else if (req.body.password === req.body.passwordConfirmation) {
-    User.create({
-      email: email,
-      password: password,
-      api_key: Math.random().toString(36).substring(2,8)
-      // the arg for toString is the radix which means what base (binary = 2); 36 includes letters
-      // substring takes the characters from index 2 to 8 not including 8
+    bcrypt.genSalt(10, function(err,salt) {
+      bcrypt.hash(req.body.password, salt, function(err,hash) {
+        User.create({
+          email: email,
+          password: hash,
+          api_key: Math.random().toString(36).substring(2,8)
+          // the arg for toString is the radix which means what base (binary = 2); 36 includes letters
+          // substring takes the characters from index 2 to 8 not including 8
+        })
+        .then(user => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(201).send(`${email} has been created`);
+        })
+        .catch(error => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(500).send({error});
+        });
+      })
     })
-    .then(user => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(201).send(`${email} has been created`);
-    })
-    .catch(error => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(500).send({error});
-    });
   }
   else {
     res.setHeader("Content-Type", "application/json");
