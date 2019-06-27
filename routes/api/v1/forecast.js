de2a4285ed1d36d08481a98e3001b85fd0125390
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-26 20:38:00 -0600

    Splits url's and adds dotenv

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch')
+require('dotenv').config()
 
 
 var City = require('../../../models').City;
@@ -8,17 +9,9 @@ var City = require('../../../models').City;
 // var CityCurrent = require('../../../models').CityCurrent;
 // var CityDays = require('../../../models').CityDays;
 
+
 const latUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=`
 const latKey = `&key=${process.env.GOOGLE_SECRET_KEY}`
-
-const steadyUrl = `https://weather.cit.api.here.com/weather/1.0/report.json?app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg&product=forecast_astronomy&name=${city}`
-const steadyKey = ``
-
-const currentUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_SECRET_KEY}/${city.coordinates}?exclude=daily,minutesly,hourly,alerts,flags`
-const darkSkyKey = ``
-
-const forecasturl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_SECRET_KEY}/#{city.coordinates}?exclude=currently,minutesly,hourly,alerts,flags&time=#{Time.now.to_f.round}`
-
 const getCityData = (input) => {
   fetch(latUrl + input + latKey)
   .then(response => response.json())
@@ -37,25 +30,16 @@ const getCityData = (input) => {
   });
 };
 
-const getNewCityForecasts = (id) => {
-  // steadies: getNewData(id,steadyUrl),
-  // current: getNewData(id,currentUrl),
-  // forecast: getNewData(id,forecastUrl)
-}
 
-const getNewSteadies = (id) => {
-  const city = City.findAll({
-    where: {
-      id: id
-    }
-  }).then(result => result.json())
-    .then(city => return city);
-  fetch(`https://weather.cit.api.here.com/weather/1.0/report.json?name=${city}&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg&product=forecast_astronomy`)
-    .then(response => response.json())
-    .then(json => {
-      return json["results"][0]["astronomy"]["astronomy"]
-    });
-};
+
+const steadyUrl = `https://weather.cit.api.here.com/weather/1.0/report.json?app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg&product=forecast_astronomy&name=${city}`
+
+const currentUrl1 = `https://api.darksky.net/forecast/${process.env.DARK_SKY_SECRET_KEY}/${city.coordinates}`
+const currentUrl2 = `?exclude=daily,minutesly,hourly,alerts,flags`
+
+const forecastUrl1 = `https://api.darksky.net/forecast/${process.env.DARK_SKY_SECRET_KEY}/${city.coordinates}`
+const forecastUrl2 = `?exclude=currently,minutesly,hourly,alerts,flags&time=${new Date()}`
+
 
 const getNewData = (id,url) => {
   const city = City.findAll({
@@ -63,12 +47,27 @@ const getNewData = (id,url) => {
       id: id
     }
   }).then(result => result.json())
-    .then(city => return `${city.name}, ${city.state}`);
+  .then(city => return `${city.name}, ${city.state}`);
   fetch(`url`)
-    .then(response => response.json())
-    .then(json => {
-      return json
-    });
+  .then(response => response.json())
+  .then(json => {
+    return json
+  });
+}
+
+const getNewCityAll = (id) => {
+  const city = City.findAll({
+    where: {
+      id: id
+    }
+  }).then(result => result.json())
+    .then(city => return city);
+  const steadyUrlFull = steadyUrl + city.name + "," + city.state;
+  const currentUrlFUll = currentUrl1 + city.lat + "," + city.long + currentUrl2;
+  const forecastUrlFUll = forecastUrl1 + city.lat + "," + city.long + forecastUrl2;
+  // steadies: getNewData(id,steadyUrlFull), ["astronomy"]["astronomy"]
+  // current: getNewData(id,currentUrlFull),
+  // forecast: getNewData(id,forecastUrlFull)
 }
 
 
@@ -88,7 +87,9 @@ router.get("/", function(req,res,next) {
   // const cityId = forecastCity["dataValues"]["id"]
   // if (forecastCity) {
   //   res.setHeader("Content-Type", "application/json");
-  //   res.status(201).send("WORDS");
+  //   res.status(201).send({
+        // getNewCityAll(forecastCity.id)
+// });
   // }
     else {
       City.create(cityData)
