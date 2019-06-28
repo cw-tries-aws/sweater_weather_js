8b8660b38b6939e48eb1943ce0abe64aa231a404
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-28 08:05:44 -0600

    Changes getCityData function to throw errors

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -15,7 +15,11 @@ const latKey = `&key=${process.env.GOOGLE_SECRET_KEY}`
 
 const getCityData = (input) => {
   return fetch(latUrl + input + latKey)
-  .then(response => response.json())
+  .then(response => {
+    if (response.ok) {
+      return response.json();}
+    throw new Error('Request Failed.');},
+    networkError => console.log(networkError.message))
   .then(json => {
     let address = json["results"][0]["formatted_address"];
     let geodata = json["results"][0]["geometry"]["location"];
@@ -26,16 +30,23 @@ const getCityData = (input) => {
       lat: geodata["lat"],
       long: geodata["lng"]
     }
-    console.log(cityData)
+    // console.log(cityData)
     return cityData
-  });
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 };
 
-console.log(getCityData(input)
-  .then(cityData => {
-    fetc(`cityData`)
-    USE THIS FOR NEXT FETCH CALL)
-)
+
+
+
+
+// console.log(getCityData(input)
+//   .then(cityData => {
+//     fetc(`cityData`)
+//     USE THIS FOR NEXT FETCH CALL)
+// )
 
 // const steadyUrl = `https://weather.cit.api.here.com/weather/1.0/report.json?app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg&product=forecast_astronomy&name=${city}`
 
@@ -46,60 +57,61 @@ console.log(getCityData(input)
 // const forecastUrl2 = `?exclude=currently,minutesly,hourly,alerts,flags&time=${new Date()}`
 
 
-const getNewData = (id,url) => {
-  const city = City.findAll({
-    where: {
-      id: id
-    }
-  }).then(result => result.json())
-  .then(city => `${city.name}, ${city.state}`);
-  fetch(`url`)
-  .then(response => response.json())
-  .then(json => {
-    return json
-  });
-}
+const getNewData = (cityData,type,url1,url2) => {
+  const latLong = cityData.lat + ',' + cityData.long
+  const fullCity = cityData.name + ',' + cityData.state
+  if (type === 'latlong') {
+    fetch(url1 + latLong + url2)
+    .then(response => response.json())
+    .then(json => {
+      return json
+    });
+  }
+  else {
+    fetch(url1 + fullCity)
+    .then(response => response.json())
+    .then(json => {
+      return json
+    });
+  }
+};
 
-const getNewCityAll = (id) => {
-  const city = City.findAll({
-    where: {
-      id: id
-    }
-  }).then(result => result.json())
-    .then(city => city);
-  const steadyUrlFull = steadyUrl + city.name + "," + city.state;
-  const currentUrlFUll = currentUrl1 + city.lat + "," + city.long + currentUrl2;
-  const forecastUrlFUll = forecastUrl1 + city.lat + "," + city.long + forecastUrl2;
+const getNewCityAll = (cityData) => {
+  const test = getNewData(cityData,steadies,steadyUrl)
+  var pry = require('pryjs'); eval(pry.it);
+  getNewData(cityData,latlong,currentUrl1,currentUrl2)
+  getNewData(cityData,latlong,forecastUrl1,forecastUrl2)
   // steadies: getNewData(id,steadyUrlFull), ["astronomy"]["astronomy"]
   // current: getNewData(id,currentUrlFull),
   // forecast: getNewData(id,forecastUrlFull)
-}
+};
 
+var pry = require('pryjs'); eval(pry.it);
 
 router.get("/", function(req,res,next) {
-  const cityData = getCityData(req.query.location)
-  // const forecastCity = City.FindAll({
-  //   where: {
-  //     name: cityData["name"],
-  //     state: cityData["state"]
-  //   }
-  // })
-  //   .then(result => result.json())
-  //   .then(json => {
-  //     return json
-  //   })
-  //
-  // const cityId = forecastCity["dataValues"]["id"]
-  // if (forecastCity) {
-  //   res.setHeader("Content-Type", "application/json");
-  //   res.status(201).send({
-        // getNewCityAll(forecastCity.id)
+//   const cityData = getCityData(req.query.location)
+//   const forecastCity = City.FindAll({
+//     where: {
+//       name: cityData["name"],
+//       state: cityData["state"]
+//     }
+//   })
+//     .then(result => result.json())
+//     .then(json => {
+//       return json
+//     })
+//
+//   const cityId = forecastCity["dataValues"]["id"]
+//   if (forecastCity) {
+//     res.setHeader("Content-Type", "application/json");
+//     res.status(201).send({
+//         getNewCityAll(forecastCity.id)
 // });
-  // }
-    // else {
-    //   City.create(cityData)
-    // }
-  // res.status(201).send("forecastCity");
+//   }
+//     else {
+//       City.create(cityData)
+//     }
+//   res.status(201).send("forecastCity");
 })
 
 module.exports = router;
