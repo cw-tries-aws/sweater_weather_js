7b1a0ae6ea2dc569f95f9cb633b246ed8d353abd
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-30 21:55:20 -0600

    Adds tests for favorites 401 errors

--- app.spec.js
+++ app.spec.js
@@ -11,21 +11,44 @@ describe("Test the root path", () => {
   })
 })
 
-describe("User Routes", () => {
-  test("it should respond to POST method", () => {
-    // add some db build up and take away here
-    // return request(app).post('/api/v1/users')
-    //   .send({api_key: "1234"})
-    //   .then(response => {
-    //     expect(response.statusCode).toBe(200)
-    //   })
-  })
-})
+// describe("User Routes", () => {
+//   test("it should respond to POST method", () => {
+//     add some db build up and take away here
+//     return request(app).post('/api/v1/users')
+//       .send({api_key: "1234"})
+//       .then(response => {
+//         expect(response.statusCode).toBe(200)
+//       })
+//   })
+// })
 
 
 describe("Forecast call", () => {
   test("it should return a 401 without an api key", () => {
-    return request(app).get('/forecast?location=salem,or')
+    return request(app).get('/api/v1/forecast?location=salem,or')
+      .then(response => {
+        expect(response.statusCode).toBe(401)
+      })
+  })
+})
+
+describe("Favorites call", () => {
+  test("get request should return a 401 without an api key", () => {
+    return request(app).get('/api/v1/favorites')
+      .then(response => {
+        expect(response.statusCode).toBe(401)
+      })
+  })
+  
+  test("post request should return a 401 without an api key", () => {
+    return request(app).post('/api/v1/favorites')
+      .then(response => {
+        expect(response.statusCode).toBe(401)
+      })
+  })
+
+  test("delete request should return a 401 without an api key", () => {
+    return request(app).delete('/api/v1/favorites')
       .then(response => {
         expect(response.statusCode).toBe(401)
       })
