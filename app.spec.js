ba560dceecd83603903d874ef3f205b63b567417
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-06-30 21:51:44 -0600

    Adds tests for root, user, and forecast get

--- app.spec.js
+++ app.spec.js
@@ -1,3 +1,33 @@
 var shell = require('shelljs')
 var request = require('supertest')
 var app = require('./app')
+
+describe("Test the root path", () => {
+  test("it should have a 200 status", () => {
+    return request(app).get('/')
+      .then(response => {
+        expect(response.statusCode).toBe(200)
+      })
+  })
+})
+
+describe("User Routes", () => {
+  test("it should respond to POST method", () => {
+    // add some db build up and take away here
+    // return request(app).post('/api/v1/users')
+    //   .send({api_key: "1234"})
+    //   .then(response => {
+    //     expect(response.statusCode).toBe(200)
+    //   })
+  })
+})
+
+
+describe("Forecast call", () => {
+  test("it should return a 401 without an api key", () => {
+    return request(app).get('/forecast?location=salem,or')
+      .then(response => {
+        expect(response.statusCode).toBe(401)
+      })
+  })
+})
